5e074c1c9e8d6b0ed4fd67270e52ced5acf31f64
Julkar Nain <julkarnain929@gmail.com>
2026-09-25 21:05:37 +0600

    navbar and footer in now global

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "FitLog — Workout Library",
@@ -10,7 +11,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-screen flex flex-col bg-base text-text font-body">
+        <Navbar />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
   const workouts = await getAllWorkouts();
   return (
     <>
-    <Navbar />
     <div className="p-10 text-shadow-green-400">
       <h1 className="text-4xl font-bold">Found {workouts.length} workouts</h1>
       <div className="mt-4 space-y-2">
@@ -20,7 +19,6 @@ export default async function Home() {
         ))}
       </div>
     </div>
-    <Footer />
     </>
   );
 }
